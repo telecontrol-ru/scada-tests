0fb3d7df93fa7d5f9fc9e8d254475ae5cb1ab211
alexsmn <alexsmn@gmail.com>
2019-09-18 21:04:43 -0600

    Update.

--- modules/scada_test.py
+++ modules/scada_test.py
@@ -1,3 +1,4 @@
+from logging import getLogger
 from scadapy import Client
 from unittest import TestCase
 import yaml
@@ -6,6 +7,12 @@ import yaml
 class BaseTest(TestCase):
 
   def setUp(self):
+    self.log = getLogger(self.__class__.__name__)
     with open("settings.yml") as f:
         settings = yaml.load(f, Loader=yaml.CLoader)
+    self.log.info("Connect to %s as %s", settings["server"], settings["user_name"])
     self.client = Client(**settings)
+
+  def tearDown(self):
+    self.log.info("Disconnect")
+    self.client.disconnect()
--- test_ping.py
+++ test_ping.py
@@ -3,7 +3,13 @@ from scada_test import BaseTest
 
 
 class PingTest(BaseTest):
-
   def test_ping(self):
-    root_folder = self.client.node(NodeId.DataItems)
-    assert root_folder.browse_name == "DataItems"
+    data_items = self.client.node(NodeId.DataItems)
+    assert data_items == NodeId.DataItems
+    assert data_items.browse_name == "DataItems"
+
+  def test_creates(self):
+    data_items = self.client.node(NodeId.DataItems)
+    data_items_creates = data_items.targets(NodeId.Creates)
+    assert NodeId.DataGroupType in data_items_creates
+    assert NodeId.DataItemType in data_items_creates
