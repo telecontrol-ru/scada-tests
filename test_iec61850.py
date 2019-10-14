22510e16c1a88d1f22e3c6455ad8dcfc2a8a1a3b
alexsmn <alexsmn@gmail.com>
2019-10-13 18:26:06 -0600

    IEC-61850: Improve test.

--- test_iec61850.py
+++ test_iec61850.py
@@ -1,8 +1,16 @@
 from scada_test import BaseTest
+import unittest
+
 
 class Iec61850Test(BaseTest):
   def test_basic(self):
     self.stack.add_from_file("test_iec61850_stack.yml")
-    device = self.stack.node("ClientDevice")
-    self.log.info("Wait for device connection")
-    device["Online"].wait_for_value(True)
+    self.log.info("Wait for client device connection")
+    self.stack.node("ClientDevice")["Online"].wait_for_value(True)
+    client_device = self.stack.node("ServerDevice")
+    self.log.info("Wait for server device connection")
+    self.stack.node("ServerDevice")["Online"].wait_for_value(True)
+
+
+if __name__ == '__main__':
+  unittest.main()
